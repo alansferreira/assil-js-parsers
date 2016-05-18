8a599ee6147d1ddfbbc52bbda7d6946c7660b125
alan.da.silva.ferreira@hotmail.com <alan.da.silva.ferreira@hotmail.com>
2016-05-18 14:52:43 -0300

    replace template engine to Mustache

--- js/index-templating.js
+++ js/index-templating.js
@@ -4,6 +4,7 @@
 /// <reference path="base.js" />
 /// <reference path="sql.ansi.parser.js" />
 /// <reference path="../lib/requirejs/require.min.js" />
+/// <reference path="../lib/mustache.js/mustache.min.js" />
 
 //////require(["jquery", "ace-editor", "angular", "angular-ui-ace", ]);
 
@@ -12,8 +13,15 @@ var app = angular.module('parserApp', ['ui.ace']);
 var ctrl = app.controller("ctrl", function ($scope) {
 
     $scope.sqlScript = $("#sampleSource").text();
-    $scope.handlebarsTemplate = $("#sampleTemplate").text();
-    $scope.handlebarsOutput = "";
+    $scope.template = '{{#databases}}' +
+                      '\n{{name}}' +
+                      '\n    {{#tables}}' +
+                      '\n        {{#schema}}{{schema}}.{{/schema}}{{name}}{{#columns}}' +
+                      '\n            {{name}} {{type}} {{#precision}}({{precision}}{{#scale}}, {{scale}}{{/scale}}){{/precision}} {{#isPrimary}}PK{{/isPrimary}}{{/columns}}' +
+                      '\n    {{/tables}}' +
+                      '\n{{/databases}}';
+
+    $scope.output = "";
 
     $scope.databases = [{name: 'sample'}];
     
@@ -23,13 +31,7 @@ var ctrl = app.controller("ctrl", function ($scope) {
 
         $scope.databases[0].tables = sql.ansi.parser.tableScript(scriptCopy);
 
-
-        var template = Handlebars.compile($scope.handlebarsTemplate);
-        $scope.handlebarsOutput = template({
-            data: { databases: $scope.databases },
-            compat: true,
-            preventIndent: true,
-        });
+        $scope.output = Mustache.render($scope.template, { databases: $scope.databases });
     };
 
     $scope.aceOptionBase = {
--- lib/handlebars-v-1-3/README.md
+++
@@ -1,4 +0,0 @@
-handlebars-v-1-3
-================
-
-Handlebars v1.3.0 & Handlebars-runtime v1.3.0 bower package
