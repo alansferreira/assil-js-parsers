9af7bb5c2bbc7ab4704c225c0ccdbb8f35747270
alan.da.silva.ferreira@hotmail.com <alan.da.silva.ferreira@hotmail.com>
2016-05-19 13:27:34 -0300

    change template engine to doT

--- js/index-templating.js
+++ js/index-templating.js
@@ -4,7 +4,7 @@
 /// <reference path="base.js" />
 /// <reference path="sql.ansi.parser.js" />
 /// <reference path="../lib/requirejs/require.min.js" />
-/// <reference path="../lib/mustache.js/mustache.min.js" />
+/// <reference path="../lib/doT/doT.min.js" />
 
 //////require(["jquery", "ace-editor", "angular", "angular-ui-ace", ]);
 
@@ -13,13 +13,19 @@ var app = angular.module('parserApp', ['ui.ace']);
 var ctrl = app.controller("ctrl", function ($scope) {
 
     $scope.sqlScript = $("#sampleSource").text();
-    $scope.template = '{{#databases}}' +
-                      '\n{{name}}' +
-                      '\n    {{#tables}}' +
-                      '\n        {{#schema}}{{schema}}.{{/schema}}{{name}}{{#columns}}' +
-                      '\n            {{name}} {{type}} {{#precision}}({{precision}}{{#scale}}, {{scale}}{{/scale}}){{/precision}} {{#isPrimary}}PK{{/isPrimary}}{{/columns}}' +
-                      '\n    {{/tables}}' +
-                      '\n{{/databases}}';
+    $scope.template =
+                        "{{~it.databases :db:dbi}}\n" +
+                        "{{=db.name}}\n" +
+                        "    {{~db.tables :tb:tbi}}{{='\\n    '}}\n" +
+                        "    {{=tb.schema}}.{{=tb.name}}\n" +
+                        "        {{~tb.columns :c:ci}}{{='\\n        '}}\n" +
+                        "        {{=c.name}} {{=c.type}} {{=c.precision}} {{=c.scale}} {{=c.isPrimary}}\n" +
+                        "        \n" +
+                        "        {{~}}\n" +
+                        "    \n" +
+                        "    {{~}}\n" +
+                        "{{~}} \n";
+
 
     $scope.output = "";
 
@@ -31,7 +37,7 @@ var ctrl = app.controller("ctrl", function ($scope) {
 
         $scope.databases[0].tables = sql.ansi.parser.tableScript(scriptCopy);
 
-        $scope.output = Mustache.render($scope.template, { databases: $scope.databases });
+        $scope.output = doT.template($scope.template)({ databases: $scope.databases });
     };
 
     $scope.aceOptionBase = {
